d7846e02559bcb678051d01a986b773cc1e71b7e
satyam-seth <satyam1998.1998@gmail.com>
2022-10-18 23:52:57 +0530

    add Chapter 3 (Functions, Classes, Routes, Theme & Text )

--- Codepur/flutter_catalog/lib/main.dart
+++ Codepur/flutter_catalog/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +11,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: HomePage(),
+      // themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        // primarySwatch: Colors.red,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
     );
   }
 }
