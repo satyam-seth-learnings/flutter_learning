d9b536f241153433516c1d40e3e6ae171b5453c5
satyam-seth <satyam1998.1998@gmail.com>
2023-03-15 22:15:51 +0530

    fix fetch data

--- Official Docs/cookbook/Networking/Fetch data from the internet/fetch_data/lib/pages/home.dart
+++ Official Docs/cookbook/Networking/Fetch data from the internet/fetch_data/lib/pages/home.dart
@@ -1,22 +1,34 @@
+import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/album.dart';
 
-class HomePage extends StatefulWidget {
-  final String title;
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 
-  const HomePage({
-    super.key,
-    required this.title,
-  });
-
-  @override
-  State<HomePage> createState() => _HomePageState();
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   late Future<Album> futureAlbum;
 
   @override
@@ -25,44 +37,33 @@ class _HomePageState extends State<HomePage> {
     futureAlbum = fetchAlbum();
   }
 
-  Future<Album> fetchAlbum() async {
-    final response = await http.get(
-      Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
-    );
-
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      return Album.fromJson(jsonDecode(response.body));
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Album>(
-      future: futureAlbum,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Scaffold(
-            body: Center(
-              child: Text(snapshot.data!.title),
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(
-              child: Text('${snapshot.error}'),
-            ),
-          );
-        }
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-        // By default, show a loading spinner.
-        return const Center(child: CircularProgressIndicator());
-      },
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
