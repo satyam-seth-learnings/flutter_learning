5ed63bacea6475550df88891c3bcd9a21c801c59
satyam-seth <satyam1998.1998@gmail.com>
2023-03-17 23:49:28 +0530

    add unit test example

---
+++ Official Docs/cookbook/Testing/Unit/An introduction to unit testing/counter_app/test/counter_test.dart
@@ -0,0 +1,35 @@
+// Import the test package and Counter class
+import 'package:counter_app/counter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
+}
