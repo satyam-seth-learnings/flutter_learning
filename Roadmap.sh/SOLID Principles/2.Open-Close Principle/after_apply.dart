242906033122cb2d2ce4b38e6d35d77bf86e2d09
satyam-seth <satyam1998.1998@gmail.com>
2023-04-07 23:08:55 +0530

    add SOLID principle examples

---
+++ Roadmap.sh/SOLID Principles/2.Open-Close Principle/after_apply.dart
@@ -0,0 +1,26 @@
+// After Apply Open-Close Principle
+
+abstract class Result {
+  checkResult();
+}
+
+class ComputerScience implements Result {
+  @override
+  checkResult() {
+    // some code
+  }
+}
+
+class Civil implements Result {
+  @override
+  checkResult() {
+    // some code
+  }
+}
+
+class Mechanical implements Result {
+  @override
+  checkResult() {
+    // some code
+  }
+}
---
+++ Roadmap.sh/SOLID Principles/3. Liskov Substitution Principle/after_apply.dart
@@ -0,0 +1,28 @@
+// After Apply Liskov Substitution Principle
+
+abstract class OfflineResult {
+  checkResult();
+}
+
+abstract class CodingResult {
+  codingTestResult();
+}
+
+class MechanicalBranch implements OfflineResult {
+  @override
+  checkResult() {
+    //  some code
+  }
+}
+
+class ComputerScienceBranch implements OfflineResult, CodingResult {
+  @override
+  checkResult() {
+    // somecode
+  }
+
+  @override
+  codingTestResult() {
+    // somecode
+  }
+}
