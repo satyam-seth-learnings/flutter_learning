23f25725072d56a1ee5c37e65d6de325bb77bde8
Satyam Seth <satyam1998.1998@gmail.com>
2026-02-21 01:36:24 +0530

    Add satefull and stateless widget example

--- Code Hero/master_flutter/lib/main.dart
+++ Code Hero/master_flutter/lib/main.dart
@@ -11,9 +11,23 @@ class NewApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.red),
+        appBar: AppBar(title: Text("Whatsapp"), backgroundColor: Colors.red),
         body: Text('Satyam'),
       ),
     );
   }
 }
+
+// class NewApp2 extends StatefulWidget {
+//   const NewApp2({super.key});
+
+//   @override
+//   State<NewApp2> createState() => _NewApp2State();
+// }
+
+// class _NewApp2State extends State<NewApp2> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
