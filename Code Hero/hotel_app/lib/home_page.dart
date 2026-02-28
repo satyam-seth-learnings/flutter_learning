e04d28daff482d3ad337e521b1f635ad8b2ff04f
Satyam Seth <satyam1998.1998@gmail.com>
2026-02-28 10:14:07 +0530

    Add hero section carousel

--- Code Hero/hotel_app/lib/home_page.dart
+++ Code Hero/hotel_app/lib/home_page.dart
@@ -5,6 +5,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> heroSectionData = ["One", "Two", "Three"];
+
     return Scaffold(
       backgroundColor: Colors.deepPurple.shade200,
       appBar: AppBar(
@@ -30,6 +32,35 @@ class HomePage extends StatelessWidget {
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(),
                 hintText: "Search here...",
+              ),
+            ),
+            SizedBox(height: 20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: heroSectionData
+                    .map(
+                      (e) => Container(
+                        margin: EdgeInsets.only(right: 10),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple.shade500,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: Text(
+                            e,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
               ),
             ),
           ],
